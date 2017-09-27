be35cbe1a5e78eac1b41f5ec3e8c06ef7d3d3f8e
creecros <creecros@gmail.com>
2017-09-27 09:06:12 -0400

    Add files via upload

---
+++ Plugin.php
@@ -0,0 +1,52 @@
+<?php
+
+
+
+namespace Kanboard\Plugin\SendEmailCreator;
+
+
+
+use Kanboard\Core\Plugin\Base;
+
+use Kanboard\Plugin\SendEmailCreator\Action\SendTaskCreator;
+
+
+
+class Plugin extends Base
+
+{
+    
+	public function initialize()
+    
+	{
+        
+		$this->actionManager->register(new SendTaskCreator($this->container));
+    
+	}
+
+	
+	public function getPluginName()	
+	{ 		 
+		return 'Auto Email Creator'; 
+	}
+
+	public function getPluginAuthor() 
+	{ 	 
+		return 'Craig Crosby'; 
+	}
+
+	public function getPluginVersion() 
+	{ 	 
+		return '0.0.1'; 
+	}
+
+	public function getPluginDescription() 
+	{ 
+		return 'Send a task by email to the creator of the task'; 
+	}
+
+	public function getPluginHomepage() 
+	{ 	 
+		return 'https://github.com/creecros/SendTaskCreator'; 
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# SendTaskCreator
-Kanboard Plugin to Send a task by email to the creator of the task.
+#Send Email to Creator
+
+
+This plugin will add a new automatic action to Kanboard. 
+
+
+This Automatic Action will allow you to send by email, a task to the creator.
+
+#Install
+Create a directory **SendEmailCreator** under the folder **plugins**
+- Copy all source files in this new directory.
+
