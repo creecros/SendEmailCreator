5368231f527facfc162c5a59d758cba5be2656b3
Ars Kurd <A.Kurdyumov@globus.ru>
2018-05-09 22:26:34 +0300

    Add ru_RU translation

---
+++ Locale/ru_RU/translations.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+  'Email subject' => 'Тема Email',
+  'Duration in days' => 'Длительность в днях',
+  'Send a task by email to creator' => 'Отправить по почте задачу создателю',
+  'Send a task by email to assignee' => 'Отправить по почте задачу ответственному',
+  'Send email notification of impending due date' => 'Отправить почтовое уведомление об истекающем сроке',
+);
