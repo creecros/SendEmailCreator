49d6417f0a724f7e09d7cda2335e54440ada7726
btd1337 <helder.bertoldo@ice.ufjf.br>
2019-04-10 14:15:44 -0300

    Add brazilian portuguese translation

---
+++ Locale/pt_BR/translations.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+  'Email subject' => 'Assunto do email',
+  'Duration in days' => 'Duração em dias',
+  'Send a task by email to creator' => 'Enviar uma tarefa por email para o seu criador',
+  'Send a task by email to assignee' => 'Enviar uma tarefa por email para o seu responsável',
+  'Send email notification of impending due date' => 'Enviar notificação por email de data de vencimento iminente',
+  'Send email notification of impending subtask due date' => 'Enviar notificação por email de data de vencimento iminente de subtarefa',
+);
