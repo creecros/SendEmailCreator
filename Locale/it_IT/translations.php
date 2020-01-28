2eac25a3f79742bb01c89f24e401507afee213ff
Valentino Pesce <valentino@iltuobrand.it>
2020-01-28 19:57:51 +0100

    Add translation for SendTaskComment

--- Locale/it_IT/translations.php
+++ Locale/it_IT/translations.php
@@ -7,4 +7,5 @@ return [
   'Send a task by email to assignee' => 'Invia un\'attività via e-mail all\'assegnatario',
   'Send email notification of impending due date' => 'Invia un\'e-mail di notifica di imminente scadenza',
   'Send email notification of impending subtask due date' => 'Invia notifica e-mail della data di scadenza della sottoattività imminente',
+  'Send new comments on a task by email' => 'Invia nuovi commenti su un\'attività via e-mail'
 ];
