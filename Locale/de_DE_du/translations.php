19c6e9a91baec7b2a81110bcf4877f3e55a084e0
Manfred Hoffmann <48651533+manne65-hd@users.noreply.github.com>
2021-04-09 14:01:36 +0200

    Added Translation "Deutsch (du)"

---
+++ Locale/de_DE_du/translations.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+  'Email subject' => 'E-Mail Betreff',
+  'Duration in days' => 'Laufzeit in Tagen',
+  'Send a task by email to creator' => 'Sende eine Aufgabe per E-Mail an den Ersteller',
+  'Send a task by email to assignee' => 'Sende eine Aufgabe per E-Mail an den Zuständigen',
+  'Send email notification of impending due date' => 'Sende eine E-Mail Benachrichtigung wenn es fällig wird',
+  'Send email notification of impending subtask due date' => 'Sende eine E-Mail Benachrichtigung wenn Teilaufgabe fällig wird',
+  'Include Task Title and ID in subject line?' => 'Ergänze Titel und ID der Aufgabe in die E-Mail Betreffzeile',
+);
